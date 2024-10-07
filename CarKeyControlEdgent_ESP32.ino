dc10864edf611040f58c87b173355db808e4ab47
Masolae Sanijehna <masolae.sa@gmail.com>
2024-10-07 13:11:11 +0700

    remove unused function

--- CarKeyControlEdgent_ESP32.ino
+++ CarKeyControlEdgent_ESP32.ino
@@ -73,53 +73,6 @@ long startPressedTime = 0;
 bool isLockButtonPressed = false;
 bool isUnLockButtonPressed = false;
 
-/*
-void setupArduinoOTA() {
-  ArduinoOTA.setPasswordHash(passWordHashed);
-  ArduinoOTA.onStart([]() {
-    Blynk.disconnect();
-    TwaiStop();
-
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH) {
-      type = "sketch";
-    } else {  // U_FS
-      type = "filesystem";
-    }
-
-    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
-    LOG_PRINT.println("Start updating " + type);
-  });
-  ArduinoOTA.onEnd([]() {
-    LOG_PRINT.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    LOG_PRINT.printf("Progress: %u%%\r\n", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    LOG_PRINT.printf("Error[%u]: ", error);
-
-    if (error == OTA_AUTH_ERROR) {
-      LOG_PRINT.println("Auth Failed");
-
-    } else if (error == OTA_BEGIN_ERROR) {
-      LOG_PRINT.println("Begin Failed");
-
-    } else if (error == OTA_CONNECT_ERROR) {
-      LOG_PRINT.println("Connect Failed");
-
-    } else if (error == OTA_RECEIVE_ERROR) {
-      LOG_PRINT.println("Receive Failed");
-
-    } else if (error == OTA_END_ERROR) {
-      LOG_PRINT.println("End Failed");
-    }
-    restartMCU();
-  });
-  ArduinoOTA.begin();
-}
-*/
-
 void RESET_ALL_KEY() {
   digitalWrite(CLOSE_DOOR_PIN, HIGH);
   digitalWrite(OPEN_DOOR_PIN, HIGH);
