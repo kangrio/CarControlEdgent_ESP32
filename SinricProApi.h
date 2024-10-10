a4e226b4231feb901fee312d17450b8e56bde656
Masolae Sanijehna <masolae.sa@gmail.com>
2024-10-10 20:13:32 +0700

    Update SinricProApi.h

--- SinricProApi.h
+++ SinricProApi.h
@@ -9,14 +9,14 @@ void registerCallbackSinricPro(CallbackFunction callback) {
 }
 
 bool onTrunkState(const String &deviceId, int &position) {
-  Serial.printf("Device %s set position to %d\r\n", deviceId.c_str(), position);
+  LOG_PRINT.printf("Device %s set position to %d\r\n", deviceId.c_str(), position);
 
   if (myCallback != NULL && (position == 0 || position == 100)) {
     bool state = true;
     if (position == 100) {
       state = false;
     }
-    timer2.setTimeout(0, [state]() {
+    timer2.setTimeout(50, [state]() {
       myCallback(state);
     });
   }
@@ -24,16 +24,17 @@ bool onTrunkState(const String &deviceId, int &position) {
   return true;  // request handled properly
 }
 
+SinricProBlinds &myTrunk = SinricPro[TRUNK_ID];
 void setupSinricPro() {
-  SinricProBlinds &myTrunk = SinricPro[TRUNK_ID];
+  // &myTrunk = SinricPro[TRUNK_ID];
   myTrunk.onRangeValue(onTrunkState);
 
   // setup SinricPro
   SinricPro.onConnected([]() {
-    Serial.printf("Connected to SinricPro\r\n");
+    LOG_PRINT.printf("Connected to SinricPro\r\n");
   });
   SinricPro.onDisconnected([]() {
-    Serial.printf("Disconnected from SinricPro\r\n");
+    LOG_PRINT.printf("Disconnected from SinricPro\r\n");
   });
   SinricPro.begin(APP_KEY, APP_SECRET);
 }
