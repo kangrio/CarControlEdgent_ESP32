a86b4444beaf4dea1938fa2db1c163011ffdf5b5
KangRio <masolae.sa@gmail.com>
2025-04-05 21:01:47 +0700

    feat: use Serial in OTA.h

--- OTA.h
+++ OTA.h
@@ -16,31 +16,31 @@ void setupArduinoOTA() {
     }
 
     // NOTE: if updating FS this would be the place to unmount FS using FS.end()
-    LOG_PRINT.println("Start updating " + type);
+    Serial.println("Start updating " + type);
   });
   ArduinoOTA.onEnd([]() {
-    LOG_PRINT.println("\nEnd");
+    Serial.println("\nEnd");
   });
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    LOG_PRINT.printf("Progress: %u%%\r\n", (progress / (total / 100)));
+    Serial.printf("Progress: %u%%\r\n", (progress / (total / 100)));
   });
   ArduinoOTA.onError([](ota_error_t error) {
-    LOG_PRINT.printf("Error[%u]: ", error);
+    Serial.printf("Error[%u]: ", error);
 
     if (error == OTA_AUTH_ERROR) {
-      LOG_PRINT.println("Auth Failed");
+      Serial.println("Auth Failed");
 
     } else if (error == OTA_BEGIN_ERROR) {
-      LOG_PRINT.println("Begin Failed");
+      Serial.println("Begin Failed");
 
     } else if (error == OTA_CONNECT_ERROR) {
-      LOG_PRINT.println("Connect Failed");
+      Serial.println("Connect Failed");
 
     } else if (error == OTA_RECEIVE_ERROR) {
-      LOG_PRINT.println("Receive Failed");
+      Serial.println("Receive Failed");
 
     } else if (error == OTA_END_ERROR) {
-      LOG_PRINT.println("End Failed");
+      Serial.println("End Failed");
     }
     restartMCU();
   });
